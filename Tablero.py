1660ea54834af8efcd5289a16a1ff5a83f13a354
OriolFarras2004 <orfasa04@gmail.com>
2020-09-09 19:42:11 +0200

    Afegint els metodes a la classe tablero i alguns detalls

--- Tablero.py
+++ Tablero.py
@@ -5,7 +5,14 @@ class Tablero():
 	def __init__(self):
 		
 		self.root = Tk()
+		
 		self.root.config(pady=10)
+		
+		self.root.title("TRES EN RALLA")
+		
+		self.root.geometry("400x500")
+		
+		self.root.resizable(width=False, height=False)
 		
 		self.img_casella_buida = PhotoImage(file="Fitxes_tres_en_ralla/res.png")
 		
@@ -71,8 +78,130 @@ class Tablero():
 		self.punts_J2.grid(row=1, column=2, padx = 30)
 		
 
+
+		self.root.mainloop()
 		
-		"""	
+	def canviar_titol(self, titol):
+		
+		self.label_titol.config(text=str(titol))
+		
+	def canviar_nom_J1(self, nom):
+		
+		self.label_J1.config(text=str(nom))
+		
+	def canviar_nom_J2(self, nom):
+		
+		self.label_J2.config(text=str(nom))
+
+	def canviar_puntuacio_J1(self, puntuacio):
+		
+		self.punts_J1.config(text=puntuacio)
+		
+	def canviar_puntuacio_empat(self, puntuacio):
+		
+		self.punts_empat.config(text=puntuacio)
+		
+	def canviar_puntuacio_J2(self, puntuacio):
+		
+		self.punts_J2.config(text=puntuacio)
+	
+	def posar_ficha(self, posicio, tipus):
+		
+		if posicio == "a1" and tipus == "x":
+			
+			self.casilla_a1.config(image= self.img_casella_creu)
+			
+		elif posicio == "a1" and tipus == "o":
+			
+			self.casilla_a1.config(image= self.img_casella_rodona)
+			
+		elif posicio == "a2" and tipus == "x":
+			
+			self.casilla_a2.config(image= self.img_casella_creu)
+			
+		elif posicio == "a2" and tipus == "o":
+			
+			self.casilla_a2.config(image= self.img_casella_rodona)
+			
+		elif posicio == "a3" and tipus == "x":
+			
+			self.casilla_a3.config(image= self.img_casella_creu)
+			
+		elif posicio == "a3" and tipus == "o":
+			
+			self.casilla_a3.config(image= self.img_casella_rodona)
+			
+		elif posicio == "b1" and tipus == "x":
+			
+			self.casilla_b1.config(image= self.img_casella_creu)
+			
+		elif posicio == "b1" and tipus == "o":
+			
+			self.casilla_b1.config(image= self.img_casella_rodona)
+			
+		elif posicio == "b2" and tipus == "x":
+			
+			self.casilla_b2.config(image= self.img_casella_creu)
+			
+		elif posicio == "b2" and tipus == "o":
+			
+			self.casilla_b2.config(image= self.img_casella_rodona)
+			
+		elif posicio == "b3" and tipus == "x":
+			
+			self.casilla_b3.config(image= self.img_casella_creu)
+			
+		elif posicio == "b3" and tipus == "o":
+			
+			self.casilla_b3.config(image= self.img_casella_rodona)
+			
+		elif posicio == "c1" and tipus == "x":
+			
+			self.casilla_c1.config(image= self.img_casella_creu)
+			
+		elif posicio == "c1" and tipus == "o":
+			
+			self.casilla_c1.config(image= self.img_casella_rodona)
+			
+		elif posicio == "c2" and tipus == "x":
+			
+			self.casilla_c2.config(image= self.img_casella_creu)
+			
+		elif posicio == "c2" and tipus == "o":
+			
+			self.casilla_c2.config(image= self.img_casella_rodona)
+			
+		elif posicio == "c3" and tipus == "x":
+			
+			self.casilla_c3.config(image= self.img_casella_creu)
+			
+		elif posicio == "c3" and tipus == "o":
+			
+			self.casilla_c3.config(image= self.img_casella_rodona)
+			
+
+
+#tablero = Tablero()
+"""
+Per a canviar el titol -> canviar_titol("Nom del titol")
+
+Per canviar nom del jugador 1 -> canviar_nom_J1("nom del jugador 1")
+
+Per canviar nom del jugador 2 -> canviar_nom_J2("nom del jugador 2")
+
+Per a canvair la puntuacio del jugador 1 -> canviar_putuacio_J1(punts)  ex: punts = 1
+
+Per a canvair la puntuacio del jugador 2 -> canviar_putuacio_J2(punts)  ex: punts = 1
+
+Per a canvair la puntuacio del empat -> canviar_putuacio_empat(punts)  ex: punts = 1
+
+Per a posar una ficha -> posar_ficha(posicio, tipus_de_ficha)  ex: posicio = "a1", tipus_de_ficha = "x" or "o" 
+
+
+"""
+
+
+"""	
 				|				|
 					|				|
 					|				|
@@ -94,7 +223,4 @@ class Tablero():
 					|				|
 					|				|
 		
-		"""
-		self.root.mainloop()
-		
-tablero = Tablero()
+"""
