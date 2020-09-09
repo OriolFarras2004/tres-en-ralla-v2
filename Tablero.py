7bf64831a8fb751632ee3671801d16ba316c38cc
OriolFarras2004 <orfasa04@gmail.com>
2020-09-09 16:45:42 +0200

    part visual completa

--- Tablero.py
+++ Tablero.py
@@ -5,6 +5,7 @@ class Tablero():
 	def __init__(self):
 		
 		self.root = Tk()
+		self.root.config(pady=10)
 		
 		self.img_casella_buida = PhotoImage(file="Fitxes_tres_en_ralla/res.png")
 		
@@ -12,7 +13,13 @@ class Tablero():
 		
 		self.img_casella_creu = PhotoImage(file="Fitxes_tres_en_ralla/creu.png")
 		
-		self.frame_casilles = Frame(self.root)
+		self.frame_titiol = Frame(self.root)
+		self.frame_titiol.pack()
+		
+		self.label_titol = Label(self.frame_titiol, text = "Tres en ralla - ", font=('Helvetica', 16, 'bold'), justify="left")
+		self.label_titol.grid(row=0,column=0, sticky="e")
+		
+		self.frame_casilles = Frame(self.root, padx=10,pady=10)
 		self.frame_casilles.pack()
 		
 		self.casilla_a1 = Button(self.frame_casilles, image = self.img_casella_buida)
@@ -41,6 +48,27 @@ class Tablero():
 		
 		self.casilla_c3 = Button(self.frame_casilles, image = self.img_casella_buida)
 		self.casilla_c3.grid(row=2, column=2)
+		
+		self.frame_informacio = Frame(self.root, padx=10, pady=20, bg = "#BFBFBF")
+		self.frame_informacio.pack()
+		
+		self.label_J1 = Label(self.frame_informacio, text = "J1", font=('Helvetica', 12, 'bold'), bg = "#BFBFBF")
+		self.label_J1.grid(row=0, column=0, padx = 30)
+		
+		self.label_empat = Label(self.frame_informacio, text = "Empats", font=('Helvetica', 12, 'bold'), bg = "#BFBFBF")
+		self.label_empat.grid(row=0, column=1, padx = 30)
+		
+		self.label_J2 = Label(self.frame_informacio, text = "J2", font=('Helvetica', 12, 'bold'), bg = "#BFBFBF")
+		self.label_J2.grid(row=0, column=2, padx = 30)
+		
+		self.punts_J1 = Label(self.frame_informacio, text = "0", bg = "#BFBFBF")
+		self.punts_J1.grid(row=1, column=0, padx = 30)
+		
+		self.punts_empat = Label(self.frame_informacio, text = "0", bg = "#BFBFBF")
+		self.punts_empat.grid(row=1, column=1, padx = 30)
+		
+		self.punts_J2 = Label(self.frame_informacio, text = "0", bg = "#BFBFBF")
+		self.punts_J2.grid(row=1, column=2, padx = 30)
 		
 
 		
